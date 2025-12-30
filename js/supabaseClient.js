f9f248fdea8e3621377cef95d8cb3a21a25baa55
NOPEfounderHD <nope.bukanguebanget@gmail.com>
2025-12-30 16:58:11 +0700

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,8 +1,8 @@
 // File: /js/supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://fuovfrdicdhnylmnacpz.supabase.co'; // Ganti dengan URL-mu
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.627V8G9x9pL8DwBjO7aP3HlM2e5qy5W6oN1E5dA2r8A'; // Ganti dengan anon key-mu
+const SUPABASE_URL = 'https://'https://fuovfrdicdhnlymnacpz.supabase.co'; // Ganti dengan URL-mu
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.'eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1b3ZmcmRpY2Robmx5bW5hY3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMjYxMzEsImV4cCI6MjA4MjYwMjEzMX0.oX4fVTEIWiRG2NaNJJKOV8dTnSHWhicLVMIFzZUl1o0'; // Ganti dengan anon key-mu
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
