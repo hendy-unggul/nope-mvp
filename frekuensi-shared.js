77a225a33bae6fd7cc47b03a4b9da89f07b2e299
NOPEfounderHD <nope.bukanguebanget@gmail.com>
2026-02-08 19:37:30 +0700

    Update frekuensi-shared.js

--- frekuensi-shared.js
+++ frekuensi-shared.js
@@ -4,7 +4,7 @@
 // ================================================
 
 const SUPABASE_URL = 'https://fuovfrdicdhnlymnacpz.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1b3ZmcmRpY2Robmx5bW5hY3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMjYxMzEsImV4cCI6MjA4MjYwMjEzMX0.oX4fVTEIWiRG2NaNJJKOV8dTnSHWhicLVMIFzZUl1o0';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1b3ZmcmRpY2Robmx5bW5hY3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMjYxMzEsImV4cCI6MjA4MjYwMjEzMX0.oX4fVTEIWiRG2NaNJJKOV8dTnSHWhicLVMIFzZUl1o0';
 
 const currentUser = localStorage.getItem('nope_username') || '@newglitch';
 
