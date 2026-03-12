432dde89d00af0af69bef5dd008e592523406943
NOPEfounderHD <nope.bukanguebanget@gmail.com>
2026-03-12 18:33:46 +0700

    Update helo.js

--- api/helo.js
+++ api/helo.js
@@ -1,3 +1,4 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Hello World' });
-}
+// Trigger new deployment
+   export default function handler(req, res) {
+     res.status(200).json({ message: 'Hello World' });
+   }
